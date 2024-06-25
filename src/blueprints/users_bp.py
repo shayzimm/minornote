d54856ea2fc2bca3653f533785cf20ee524ac077
shayzimm <shay.zimmerle@gmail.com>
2024-06-25 10:26:56 +0800

    applied CRUD to users_bp

--- src/blueprints/users_bp.py
+++ src/blueprints/users_bp.py
@@ -1,12 +1,13 @@
 from datetime import timedelta
 from flask import request, Blueprint
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required
 from models.user import User, UserSchema
 from auth import admin_only
 from init import db, bcrypt
 
 users_bp = Blueprint('users', __name__, url_prefix='/users')
 
+#  Get all users (R)
 @users_bp.route('/')
 @admin_only
 def all_users():
@@ -14,11 +15,13 @@ def all_users():
     users = db.session.scalars(stmt).all()
     return UserSchema(many=True).dump(users)
 
+# Get one user (R)
 @users_bp.route('/<int:id>')
 def one_user(id):
     user = db.get_or_404(User, id)
     return UserSchema().dump(user)
 
+# Login (C)
 @users_bp.route('/login', methods=['POST'])
 def login():
     # email = request.json['email']
@@ -32,3 +35,43 @@ def login():
     else:
         return {'error': 'Invalid email or password'}, 401
     
+# Create new user - Registration (C)
+@users_bp.route('/register', methods=['POST'])
+def create_user():
+    user_info = UserSchema(only=['username', 'email', 'password', 'first_name', 'last_name', 'is_admin']).load(
+        request.json)
+    user = User(
+        username=user_info['username'],
+        email=user_info['email'],
+        password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+        first_name=user_info['first_name'],
+        last_name=user_info['last_name'],
+        is_admin=user_info.get('is_admin', False)
+    )
+    db.session.add(user)
+    db.session.commit()
+    return UserSchema().dump(user), 201
+
+# Update a user (U)
+@users_bp.route('/<int:id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_user(id):
+    user = db.get_or_404(User, id)
+    user_info = UserSchema(only=['username', 'email', 'password', 'first_name', 'last_name', 'is_admin']).load(
+        request.json, unknown='exclude')
+    user.username = user_info.get('username', user.username)
+    user.email = user_info.get('email', user.email)
+    user.password = user_info.get('password', user.password)
+    user.first_name = user_info.get('first_name', user.first_name)
+    user.last_name = user_info.get('last_name', user.last_name)
+    db.session.commit()
+    return UserSchema().dump(user)
+
+# Delete a user (D)
+@users_bp.route('/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_user(id):
+    user = db.get_or_404(User, id)
+    db.session.delete(user)
+    db.session.commit()
+    return {}, 204
