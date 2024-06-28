ab55a587dda8df44540277bb6e3468c51d99e7ed
shayzimm <shay.zimmerle@gmail.com>
2024-06-28 17:57:17 +0800

    commented init.py

--- src/init.py
+++ src/init.py
@@ -6,16 +6,26 @@ from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
+# Define the base class for SQLAlchemy models
 class Base(DeclarativeBase):
     pass
 
+# Initialise the Flask application
 app = Flask(__name__)
 
-app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_KEY')
+# Configure the application with environment variables
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY') # Secret key for JWT
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_KEY') # Database URI for SQLAlchemy
 
+# Initialise the SQLAlchemy instance
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
+# Initialise the Marshmallow instance for serialisation and deserialization
 ma = Marshmallow(app)
+
+# Initialise Bcrypt instance for password hashing and salting
 bcrypt = Bcrypt(app)
+
+# Initialise JWT manager for managing JWT tokens and user authentication
 jwt = JWTManager(app)
